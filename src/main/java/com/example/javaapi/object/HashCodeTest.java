005c9c25e3fe0c1437f06103037c64f01e6acb18
PARKCY\Park <mangoperry2015@gmail.com>
2024-06-08 12:50:17 +0900

    [String] HashCodeTest 주석 수정

--- src/main/java/com/example/javaapi/object/HashCodeTest.java
+++ src/main/java/com/example/javaapi/object/HashCodeTest.java
@@ -2,7 +2,8 @@ package com.example.javaapi.object;
 
 public class HashCodeTest {
 
-    /** Hash Code : 객체를 식별하는 값, 객체의 메모리 주소값
+    /** Hash Code
+     *  객체를 식별하는 값, 객체의 메모리 주소값
      *  equals()를 재정의하는 경우 반드시 hashCode() 재정의 필요 (Object 클래스 명세 참조)
      *  따라서 동등 값을 가지는 동등 객체는 같은 해시코드를 가져야 함
      */
